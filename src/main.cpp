e9c7ecafdee691de315cc019255d4b97bf48dd3d
jammes3 <jammesduran3@gmail.com>
2022-09-19 17:30:23 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(921600);
+  Serial.println("Bienvenido Esta funcionando y programado desde vs code con platformIO");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
